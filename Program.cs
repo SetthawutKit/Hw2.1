54984ed43cb7be6c90cf3a9d01ef4cab6a26d7ee
SetthawutKit <122444852+SetthawutKit@users.noreply.github.com>
2023-05-20 00:08:32 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿class Program{
+    static void Main(string[] args){
+        Console.Write("Enter the row number of Pascal's triangle: ");
+        int row = int.Parse(Console.ReadLine());
+
+        if (row < 0){
+            Console.WriteLine("Invalid Pascal's triangle row number.");
+            Console.Write("Enter the row number of Pascal's triangle: ");
+            row = int.Parse(Console.ReadLine());
+        }
+
+        for (int i = 0; i <= row; i++){
+            int c = 1;
+            for (int j = 0; j <= i; j++){
+                Console.Write("{0} ", c);
+                c = c * (i - j) / (j + 1);
+            }
+            Console.WriteLine();
+        }
+    }
+}
